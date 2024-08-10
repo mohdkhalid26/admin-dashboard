f4a9cc8d775b5b89e77e5dc23c6a9f18078b73c0
FaisalAnsari <faisalansari30121998@gmail.com>
2024-08-10 22:34:50 +0530

    implemented/Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import SignIn from './modules/signIn'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-<div>
-  Khalid Bhaukaal
-</div>
+    <Routes>
+      <Route path='sign_in' element={<SignIn />} />
+    </Routes>
   )
 }
 
